f5894766da2ef299be9ec5976d9e739c1f60d254
coulsibou123 <coulsibou123@gmail.com>
2025-01-14 11:58:39 +0000

    commit

---
+++ src/main/java/sn/ods/starterkit_spring/infrastructure/logging/ErrorLog.java
@@ -0,0 +1,33 @@
+package sn.ods.starterkit_spring.infrastructure.logging;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class ErrorLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String serviceName;       // Nom du service où l'erreur s'est produite
+    private String methodName;        // Nom de la méthode où l'erreur s'est produite
+
+    private String errorMessage;      // Message d'erreur
+
+    //private String stackTrace;
+    // Stack trace de l'erreur
+    private LocalDateTime timestamp;  // Date et heure de l'erreur
+
+    public ErrorLog() {
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public ErrorLog(String serviceName, String methodName, String errorMessage) {
+        this.serviceName = serviceName;
+        this.methodName = methodName;
+        this.errorMessage = errorMessage;
+
+        this.timestamp = LocalDateTime.now();
+    }
+}
--- src/main/java/sn/ods/starterkit_spring/infrastructure/logging/ErrorLogRepository.java
+++ src/main/java/sn/ods/starterkit_spring/infrastructure/logging/ErrorLogRepository.java
@@ -2,5 +2,5 @@ package sn.ods.starterkit_spring.infrastructure.logging;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LogRepository extends JpaRepository<LogEntry, Long> {
+public interface ErrorLogRepository extends JpaRepository<ErrorLog, Long> {
 }
