19ac3e44712199420fba0cbe76174461f701334d
coulsibou123 <coulsibou123@gmail.com>
2025-01-16 14:48:13 +0000

    cccccc

--- src/main/java/sn/ods/starterkit_spring/StarterkitSpringApplication.java
+++ src/main/java/sn/ods/starterkit_spring/StarterkitSpringApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+
 
 import java.util.concurrent.Executors;
 
 @SpringBootApplication
-//@ComponentScan(basePackages = {"sn.ods.starterkit_spring.infrastructure.logging"})
 public class StarterkitSpringApplication {
 
 	public static void main(String[] args) {
--- src/main/java/sn/ods/starterkit_spring/infrastructure/logging/GlobalExceptionHandler.java
+++ src/main/java/sn/ods/starterkit_spring/infrastructure/logging/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package sn.ods.starterkit_spring.infrastructure.logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
